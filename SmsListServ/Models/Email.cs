462ecdf1f2c73da8f73f7bb6c9e63a017d1cb0a0
Stephen Dolenc <sdolenc@users.noreply.github.com>
2018-08-22 08:13:14 -0700

    First change (#1)
    * basics
    
    * possible models
    
    * first mock

---
+++ SmsListServ/Models/Email.cs
@@ -0,0 +1,48 @@
+﻿// json2csharp
+namespace Models.Email
+{
+    using System;
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+
+    public class RootObject
+    {
+        public string uri { get; set; }
+        public string method { get; set; }
+        public Body body { get; set; }
+    }
+
+    public class Attachment
+    {
+        public string Name { get; set; }
+        public string ContentBytes { get; set; }
+        public string ContentType { get; set; }
+        public string ContentId { get; set; }
+        public int Size { get; set; }
+    }
+
+    public class Body
+    {
+        public bool MultipleEmailsFound { get; set; }
+        public string From { get; set; }
+        public string SenderName { get; set; }
+        public string To { get; set; }
+        public string Cc { get; set; }
+        public string Bcc { get; set; }
+        public string Subject { get; set; }
+
+        [JsonProperty("Body")]
+        public string EmailBody { get; set; }
+        public string Snippet { get; set; }
+        public List<string> LabelIds { get; set; }
+        public int HistoryId { get; set; }
+        public DateTime DateTimeReceived { get; set; }
+        public int EstimatedSize { get; set; }
+        public bool IsRead { get; set; }
+        public bool IsHtml { get; set; }
+        public bool HasAttachments { get; set; }
+        public List<Attachment> Attachments { get; set; }
+        public string Id { get; set; }
+        public string ThreadId { get; set; }
+    }
+}
---
+++ SmsListServ/Models/Email2.cs
@@ -0,0 +1,96 @@
+﻿// quicktype
+namespace Models.Email2
+{
+    using System;
+    using Newtonsoft.Json;
+
+    public partial class Welcome
+    {
+        [JsonProperty("uri")]
+        public string Uri { get; set; }
+
+        [JsonProperty("method")]
+        public string Method { get; set; }
+
+        [JsonProperty("body")]
+        public Body Body { get; set; }
+    }
+
+    public partial class Attachment
+    {
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+
+        [JsonProperty("ContentBytes")]
+        public string ContentBytes { get; set; }
+
+        [JsonProperty("ContentType")]
+        public string ContentType { get; set; }
+
+        [JsonProperty("ContentId")]
+        public string ContentId { get; set; }
+
+        [JsonProperty("Size")]
+        public long Size { get; set; }
+    }
+
+public partial class Body
+    {
+        [JsonProperty("MultipleEmailsFound")]
+        public bool MultipleEmailsFound { get; set; }
+
+        [JsonProperty("From")]
+        public string From { get; set; }
+
+        [JsonProperty("SenderName")]
+        public string SenderName { get; set; }
+
+        [JsonProperty("To")]
+        public string To { get; set; }
+
+        [JsonProperty("Cc")]
+        public string Cc { get; set; }
+
+        [JsonProperty("Bcc")]
+        public string Bcc { get; set; }
+
+        [JsonProperty("Subject")]
+        public string Subject { get; set; }
+
+        [JsonProperty("Body")]
+        public string EmailBody { get; set; }
+
+        [JsonProperty("Snippet")]
+        public string Snippet { get; set; }
+
+        [JsonProperty("LabelIds")]
+        public string[] LabelIds { get; set; }
+
+        [JsonProperty("HistoryId")]
+        public long HistoryId { get; set; }
+
+        [JsonProperty("DateTimeReceived")]
+        public DateTimeOffset DateTimeReceived { get; set; }
+
+        [JsonProperty("EstimatedSize")]
+        public long EstimatedSize { get; set; }
+
+        [JsonProperty("IsRead")]
+        public bool IsRead { get; set; }
+
+        [JsonProperty("IsHtml")]
+        public bool IsHtml { get; set; }
+
+        [JsonProperty("HasAttachments")]
+        public bool HasAttachments { get; set; }
+
+        [JsonProperty("Attachments")]
+        public Attachment[] Attachments { get; set; }
+
+        [JsonProperty("Id")]
+        public string Id { get; set; }
+
+        [JsonProperty("ThreadId")]
+        public string ThreadId { get; set; }
+    }
+}
